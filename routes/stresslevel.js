3c548fa25cdd22a0c137bd070a52d2f59bd322e2
rizky-pramudita <rzkprmdt@gmail.com>
2023-06-12 21:31:51 +0700

    hitungan

--- routes/stresslevel.js
+++ routes/stresslevel.js
@@ -4,34 +4,23 @@ const connection = require('../database');
 
 // Endpoint untuk menghitung stress level mingguan
 router.get('/weekly/:id', async (req, res) => {
- 
   const id = req.params.id;
 
   try {
-    const weekly = `SElECT AVG(stress_value) from stress_level WHERE employee_id = $1 GROUP BY date`;
-    const avg = `SElECT AVG(stress_value) from stress_level WHERE employee_id = $1 GROUP BY employee_id`;
-    await connection.query(weekly, [id], (error, resultWeekly)=> {
-      if (error){
-          console.error(error);
-          res.status(500).json({message: 'Internal Server Error'})
-      }
-      connection.query(avg, [id], (error, resultAvg)=> {
-        if (error){
-            console.error(error);
-            res.status(500).json({message: 'Internal Server Error'})
-        }
-      const response = {
-          weekly_avg: resultWeekly.rows,
-          total_avg: resultAvg.rows
-      }
-        res.status(200).json(response);
-    })
-  })
+    const avg = 'SELECT AVG(stress_value) AS average_value FROM stress_level WHERE employee_id=$1';
+    const resultAvg = await connection.query(avg, [id]);
+    const stressPrecentage = resultAvg.rows[0].average_value/40*100;
+    const response = {
+      total_avg: stressPrecentage
+    };
+
+    res.status(200).json(response);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
   }
 });
+
 
 
 // Endpoint untuk menyimpan stress level mingguan
