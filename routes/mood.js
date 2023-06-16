8f40c882ec0366125ccd6351f5e74b3d1e725039
rizky-pramudita <rzkprmdt@gmail.com>
2023-06-16 18:17:30 +0700

    fixed endpoint

--- routes/mood.js
+++ routes/mood.js
@@ -3,11 +3,50 @@ const router = express.Router();
 const connection = require('../database');
 
 
+// router.get('/weekly-calendar/:id', async (req, res) => {
+//     const id = req.params.id;
+//     const sqlCalendar = `SELECT mood_value, date, employee_id FROM mood WHERE employee_id=$1 AND date BETWEEN current_date - interval '7' day AND current_date ORDER BY date ASC`;
+//     const sqlStressWeek = `SELECT AVG(stress_value) AS stress_value FROM stress_level WHERE employee_id=$1 AND date BETWEEN current_date - interval '7' day AND current_date ORDER BY date ASC`;
+//     const sqlMoodWeek = `SELECT mood_value FROM mood WHERE employee_id = $1 AND date BETWEEN current_date - interval '7' day AND current_date GROUP BY mood_value ORDER BY COUNT(*) DESC LIMIT 1;`;
+  
+//     try {
+//       const resultCalendar = await connection.query(sqlCalendar, [id]);
+//       const resultStressWeek = await connection.query(sqlStressWeek, [id]);
+//       const resultMoodWeek = await connection.query(sqlMoodWeek, [id]);
+  
+//       const counts = resultCalendar.rows.reduce((acc, obj) => {
+//         const key = obj.date;
+//         acc[key] = (acc[key] || 0) + 1;
+//         return acc;
+//       }, {});
+  
+//       const uniqueArray = Object.values(resultCalendar.rows.reduce((acc, obj) => {
+//         const key = obj.date;
+//         if (!acc[key] || acc[key].count < counts[key]) {
+//           acc[key] = obj;
+//           acc[key].count = counts[key];
+//         }
+//         return acc;
+//       }, []));
+  
+//       const response = {
+//         weekly_stress_avg: resultStressWeek.rows.length > 0 ? resultStressWeek.rows[0].stress_value : null,
+//         weekly_mood: resultMoodWeek.rows.length > 0 ? resultMoodWeek.rows[0].mood_value : null,
+//         weekly_calendar: uniqueArray,
+//       };
+  
+//       res.status(200).json(response);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ message: 'Internal Server Error' });
+//     }
+//   });
+
 router.get('/weekly-calendar/:id', async (req, res) => {
     const id = req.params.id;
     const sqlCalendar = `SELECT mood_value, date, employee_id FROM mood WHERE employee_id=$1 AND date BETWEEN current_date - interval '7' day AND current_date ORDER BY date ASC`;
-    const sqlStressWeek = `SELECT AVG(stress_value) AS stress_value FROM stress_level WHERE employee_id=$1 AND date BETWEEN current_date - interval '7' day AND current_date ORDER BY date ASC`;
-    const sqlMoodWeek = `SELECT mood_value FROM mood WHERE employee_id = $1 AND date BETWEEN current_date - interval '7' day AND current_date GROUP BY mood_value ORDER BY COUNT(*) DESC LIMIT 1;`;
+    const sqlStressWeek = `SELECT AVG(stress_value) AS stress_value FROM stress_level WHERE employee_id=$1 AND date BETWEEN current_date - interval '7' day AND current_date`;
+    const sqlMoodWeek = `SELECT mood_value FROM mood WHERE employee_id = $1 AND date BETWEEN current_date - interval '7' day AND current_date GROUP BY mood_value ORDER BY COUNT(*) DESC LIMIT 1`;
   
     try {
       const resultCalendar = await connection.query(sqlCalendar, [id]);
@@ -15,19 +54,19 @@ router.get('/weekly-calendar/:id', async (req, res) => {
       const resultMoodWeek = await connection.query(sqlMoodWeek, [id]);
   
       const counts = resultCalendar.rows.reduce((acc, obj) => {
-        const key = obj.date;
+        const key = obj.date.toISOString().split('T')[0];
         acc[key] = (acc[key] || 0) + 1;
         return acc;
       }, {});
   
       const uniqueArray = Object.values(resultCalendar.rows.reduce((acc, obj) => {
-        const key = obj.date;
+        const key = obj.date.toISOString().split('T')[0];
         if (!acc[key] || acc[key].count < counts[key]) {
           acc[key] = obj;
           acc[key].count = counts[key];
         }
         return acc;
-      }, []));
+      }, {}));
   
       const response = {
         weekly_stress_avg: resultStressWeek.rows.length > 0 ? resultStressWeek.rows[0].stress_value : null,
@@ -42,6 +81,7 @@ router.get('/weekly-calendar/:id', async (req, res) => {
     }
   });
   
+
 
 router.post('/add/:id', async (req, res) => {
     const randomArray = ["joy", "sadness", "anger", "fear", "love"];
