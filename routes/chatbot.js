ffa7e332f21b2aaedd2b78a3b941cc726747ad70
rizky-pramudita <rzkprmdt@gmail.com>
2023-06-16 18:40:27 +0700

    flag

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -13,25 +13,30 @@ router.post('/chatbot/:id', async (req, res) => {
     let botAnswer;
     formDataMood.append('data', chat);
     formData.append('msg', chat);
-  
+    console.log("flag1");
     try {
       // Make the chatbot API request using Axios
       const chatbotResponse = await axios.post(chatbotUrl, formData);
       botAnswer = chatbotResponse.data;
 
+        console.log("flag2");
       // Make the mood API request using Axios
       const moodResponse = await axios.post(moodUrl, formDataMood);
       const mood = moodResponse.data;
       // Perform the database insertion
+
+     console.log("flag3");
       const sql = 'INSERT INTO mood (employee_id, date, mood_value) VALUES ($1, current_date, $2)';
 
       await client.query(sql, [employee_id, mood.data]);
         
+     console.log("flag4");
       // Send the response with the bot answer
       const answer = {
         message: 'Mood Added',
         answer: botAnswer,
       };
+      console.log("flag5");
       res.status(200).json(answer);
     } catch (error) {
       console.error(error);
